8cc6fe07b40d91f0efdd0c85cb086301d6f84d67
Jeremiah Gage <jeremiahdgage@outlook.com>
2026-07-27 22:01:46 -0500

    Add favicon.

--- resources/views/saints/index.blade.php
+++ resources/views/saints/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $saint->primary_name }} | {{ config('app.name', 'Ambry') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     @vite([
         'resources/css/app.css',
         'resources/css/saints/index.css',
--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Ambry') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     @vite([
         'resources/css/app.css',
         'resources/css/search/index.css',
